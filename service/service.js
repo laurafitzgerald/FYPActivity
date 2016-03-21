b66fbff788d3f3cdd073c5de9c5314fae291aead
Laura Fitzgerald <laurafitzgeraldsemail@gmail.com>
2016-03-21 15:52:39 +0000

    Implement nats

--- service/service.js
+++ service/service.js
@@ -4,12 +4,17 @@ var nc = nats.connect({'servers': servers});
 console.log("Connected to " + nc.currentServer);
 
 
-var activitytable = '{ "columns": { "timestamp": "text", "type": "text" ,"time": "decimal", "distance": "decimal", "speed": "decimal", "username": "text"  },"primary": ["timestamp","username"],"indexes": ["username"]}';
+var activitytable = '{ "columns": { "name": "text","timestamp": "text", "type": "text" ,"time": "decimal", "distance": "decimal", "speed": "decimal", "username": "text"  },"primary": ["timestamp","username"],"indexes": ["username"]}';
 nc.request('data.register.activity', activitytable, function(response){});
 
+
+
+
 nc.subscribe('activity.read.byuser', function(msg, reply, sub){
-		console.log("received read by user request");
-		nc.request('data.query.activity',msg, function(response){
+		var username = "'" +msg + "'";
+		var filter = '"username='+username+ '"';
+		var msg = '{ "filters" : [ ' + filter + ']}';
+		nc.request('data.query.activity', msg, function(response){
 			//read records from the activity table using the user id
 			console.log(response);
 			//results = JSON.parse(response);
@@ -21,12 +26,14 @@ nc.subscribe('activity.read.byuser', function(msg, reply, sub){
 //msg = data
 nc.subscribe('activity.create', function(msg, reply, sub){
 		console.log("Activity.create");
-		console.log("Message.username: " + msg);
-			console.log("valid user found in activity.create");
+		//console.log("Message.username: " + msg);
+			//console.log("valid user found in activity.create");
 			nc.request('data.write.activity', msg, function(response){
 				//will need to do error handling/other stuff here
 				//making sure that user names are correctly formatted etc.
 				//passwords are okay etc. 
+				console.log("Response : " + response)
+				console.log("Reply : " + reply);
 				nc.publish(reply,response);
 			});
 	
@@ -35,10 +42,17 @@ nc.subscribe('activity.create', function(msg, reply, sub){
 nc.subscribe('activity.delete', function(msg,reply,sub){
 	//delete by username and timestamp
 	//needs to be the composite key
-	var uuid = "'" + msg + "'";
-	var filter = '"uuid='+uuid + '"';
-	var msg = '{ "filters" : [ ' + filter + ']}';
-	nc.request('data.delete.activity', msg, function(response){
+	//var uuid = "'" + msg + "'";
+	//var filter = '"uuid='+uuid + '"';
+	//var msg = '{ "filters" : [ ' + filter + ']}';
+	var msg = JSON.parse(msg);
+	console.log(msg.username);
+	console.log(msg.timestamp);
+
+	var username = "username='" +msg.username +"'";
+	var timestamp = "timestamp='" + msg.timestamp +"'";
+	var filter  = { "filters" : [ username , timestamp]};
+	nc.request('data.delete.activity', JSON.stringify(filter) , function(response){
 		nc.publish(reply, response);
 	});
 
