fb99c709d6fe7744c2739f66aa896c48e546820d
Laura Fitzgerald <laurafitzgeraldsemail@gmail.com>
2016-04-03 18:02:42 +0100

    Fixed issue on search

--- service/service.js
+++ service/service.js
@@ -4,7 +4,7 @@ var nc = nats.connect({'servers': servers});
 console.log("Connected to " + nc.currentServer);
 
 
-var activitytable = '{ "columns": { "name": "text","timestamp": "text", "type": "text" ,"time": "decimal", "distance": "decimal", "speed": "decimal", "username": "text"  },"primary": ["timestamp","username"],"indexes": ["username"]}';
+var activitytable = '{ "columns": { "id": "text", "name": "text","timestamp": "text", "type": "text" ,"location": "text" ," time": "decimal", "distance": "decimal", "speed": "decimal", "username": "text"  },"primary": ["id"], "indexes": ["username"]}';
 nc.request('data.register.activity', activitytable, function(response){});
 
 
@@ -15,27 +15,31 @@ nc.subscribe('activity.read.byuser', function(msg, reply, sub){
 		var filter = '"username='+username+ '"';
 		var msg = '{ "filters" : [ ' + filter + ']}';
 		nc.request('data.query.activity', msg, function(response){
-			//read records from the activity table using the user id
 			console.log(response);
-			//results = JSON.parse(response);
-			//result = JSON.stringify(results);
 			nc.publish(reply,response);
 		});
 });
 
 //msg = data
 nc.subscribe('activity.create', function(msg, reply, sub){
-		console.log("Activity.create");
-		//console.log("Message.username: " + msg);
-			//console.log("valid user found in activity.create");
-			nc.request('data.write.activity', msg, function(response){
-				//will need to do error handling/other stuff here
-				//making sure that user names are correctly formatted etc.
-				//passwords are okay etc. 
-				console.log("Response : " + response)
-				console.log("Reply : " + reply);
-				nc.publish(reply,response);
-			});
+	console.log("Activity.create");
+	//console.log("Message.username: " + msg);	
+	var obj = JSON.parse(msg);
+	var d = new Date();
+	var currentdate = d.toLocaleString();
+	console.log("Current Date: "+ currentdate);
+	obj.timestamp = currentdate;
+	obj.id = (new Date).getTime().toString();
+
+
+	nc.request('data.write.activity', JSON.stringify(obj), function(response){
+		//will need to do error handling/other stuff here
+		//making sure that user names are correctly formatted etc.
+		//passwords are okay etc. 
+		console.log("Response : " + response)
+		console.log("Reply : " + reply);
+		nc.publish(reply,response);
+	});
 	
 });
 
@@ -45,14 +49,16 @@ nc.subscribe('activity.delete', function(msg,reply,sub){
 	//var uuid = "'" + msg + "'";
 	//var filter = '"uuid='+uuid + '"';
 	//var msg = '{ "filters" : [ ' + filter + ']}';
-	var msg = JSON.parse(msg);
-	console.log(msg.username);
-	console.log(msg.timestamp);
 
-	var username = "username='" +msg.username +"'";
-	var timestamp = "timestamp='" + msg.timestamp +"'";
-	var filter  = { "filters" : [ username , timestamp]};
-	nc.request('data.delete.activity', JSON.stringify(filter) , function(response){
+	var query = {};
+	var msg = msg.replace(/"/g, '');
+	
+
+	query.filters = ["id='" + msg +"'"];
+
+	console.log(" query : " + JSON.stringify(query));
+	nc.request('data.delete.activity', JSON.stringify(query) , function(response){
+		console.log("delete response" + response);
 		nc.publish(reply, response);
 	});
 
